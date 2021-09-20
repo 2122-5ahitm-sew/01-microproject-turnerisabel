007a81790ee9f9d3d812153edf7da4e3b6bf871b
turnerisabel <turner.isabel03@gmail.com>
2021-09-20 20:27:40 +0200

    Turner-implemented variables, constructor, equals&hashCode and toString() in class Bus

--- backend/src/main/java/at/htl/busreisen/entity/Bus.java
+++ backend/src/main/java/at/htl/busreisen/entity/Bus.java
@@ -1,4 +1,37 @@
 package at.htl.busreisen.entity;
 
+import java.util.Objects;
+
 public class Bus {
+
+    public Long id;
+    public int seats;
+
+    public Bus() {
+    }
+
+    public Bus(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bus bus = (Bus) o;
+        return seats == bus.seats && Objects.equals(id, bus.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, seats);
+    }
+
+    @Override
+    public String toString() {
+        return "Bus{" +
+                "id=" + id +
+                ", seats=" + seats +
+                '}';
+    }
 }
