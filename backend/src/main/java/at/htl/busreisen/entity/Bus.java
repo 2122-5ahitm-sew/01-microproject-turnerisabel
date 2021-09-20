6559c84e336d37b0d1285fd5409a436961931e8c
turnerisabel <turner.isabel03@gmail.com>
2021-09-20 20:35:31 +0200

    Turner-added annotations in class Bus

--- backend/src/main/java/at/htl/busreisen/entity/Bus.java
+++ backend/src/main/java/at/htl/busreisen/entity/Bus.java
@@ -1,10 +1,18 @@
 package at.htl.busreisen.entity;
 
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Bus {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+
     public int seats;
 
     public Bus() {
