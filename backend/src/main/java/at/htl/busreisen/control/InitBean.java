7a8cd97d12392defdbb31b30872b79f47e3dc922
turnerisabel <turner.isabel03@gmail.com>
2021-10-12 14:58:22 +0200

    Turner-implemented InitBean

--- backend/src/main/java/at/htl/busreisen/control/InitBean.java
+++ backend/src/main/java/at/htl/busreisen/control/InitBean.java
@@ -1,4 +1,31 @@
 package at.htl.busreisen.control;
 
+import at.htl.busreisen.entity.Bus;
+import at.htl.busreisen.entity.Destination;
+import at.htl.busreisen.entity.Person;
+import io.quarkus.runtime.StartupEvent;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 public class InitBean {
+
+    @Inject
+    EntityManager entityManager;
+
+    @Transactional
+    public void init(@Observes StartupEvent ev){
+        // String street, int streetNo, String place, int zip
+        Destination destination = new Destination("ExampleStreet", 10, "ExamplePlace", 1234);
+
+        entityManager.merge(destination);
+
+        Bus bus1 = new Bus(50);
+        // Person
+        Person person = new Person("Susi", "Muster", "0667/12345678", bus1);
+
+        entityManager.merge(person);
+    }
 }
