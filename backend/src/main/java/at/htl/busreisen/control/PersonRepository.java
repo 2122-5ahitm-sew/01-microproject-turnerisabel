b523c0349e916a39bd2cdd79ac8064a63ae6b49c
turnerisabel <turner.isabel03@gmail.com>
2021-10-12 14:56:40 +0200

    Turner-implemented PersonRepository

--- backend/src/main/java/at/htl/busreisen/control/PersonRepository.java
+++ backend/src/main/java/at/htl/busreisen/control/PersonRepository.java
@@ -1,4 +1,10 @@
 package at.htl.busreisen.control;
 
-public class PersonRepository {
+import at.htl.busreisen.entity.Person;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class PersonRepository implements PanacheRepository<Person> {
 }
