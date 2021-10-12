07889895b24a045bbdaba38bb582ecf85b972176
turnerisabel <turner.isabel03@gmail.com>
2021-10-12 14:57:19 +0200

    Turner-implemented TripRepository

--- backend/src/main/java/at/htl/busreisen/control/TripRepository.java
+++ backend/src/main/java/at/htl/busreisen/control/TripRepository.java
@@ -1,4 +1,10 @@
 package at.htl.busreisen.control;
 
-public class TripRepository {
+import at.htl.busreisen.entity.Trip;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class TripRepository implements PanacheRepository<Trip> {
 }
