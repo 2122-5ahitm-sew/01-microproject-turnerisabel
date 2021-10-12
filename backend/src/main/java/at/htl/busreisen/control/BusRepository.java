230335e5f760a9af39cbefba362179798b571620
turnerisabel <turner.isabel03@gmail.com>
2021-10-12 14:55:17 +0200

    Turner-implemented BusRepository

--- backend/src/main/java/at/htl/busreisen/control/BusRepository.java
+++ backend/src/main/java/at/htl/busreisen/control/BusRepository.java
@@ -1,4 +1,10 @@
 package at.htl.busreisen.control;
 
-public class BusRepository {
+import at.htl.busreisen.entity.Bus;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class BusRepository implements PanacheRepository<Bus> {
 }
