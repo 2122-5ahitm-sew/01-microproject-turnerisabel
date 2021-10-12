8edc9fce32c90dfe823c99cc2731d74a0f6bb9b0
turnerisabel <turner.isabel03@gmail.com>
2021-10-12 14:54:31 +0200

    Turner-implemented BookingRepository

--- backend/src/main/java/at/htl/busreisen/control/BookingRepository.java
+++ backend/src/main/java/at/htl/busreisen/control/BookingRepository.java
@@ -1,4 +1,11 @@
 package at.htl.busreisen.control;
 
-public class BookingRepository {
+import at.htl.busreisen.entity.Booking;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationScoped
+public class BookingRepository implements PanacheRepository<Booking> {
 }
--- backend/src/main/java/at/htl/busreisen/control/InitBean.java
+++ backend/src/main/java/at/htl/busreisen/control/InitBean.java
@@ -1,4 +1,4 @@
 package at.htl.busreisen.control;
 
-public class InitBean{
+public class InitBean {
 }
